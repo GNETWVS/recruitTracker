8f3439f5e402ce10eee0031606e8c604d8db7bb4
dakkaynak <derya.akkaynak@gmail.com>
2017-05-31 16:49:24 +0300

    Delete filterCoralsbySizeInImage.m

--- filterCoralsbySizeInImage.m
+++
@@ -1,6 +0,0 @@
-function coralImage = filterCoralsbySizeInImage(Ibw,cmThreshold,pixToCM)
-
-stats = regionprops(Ibw,'MajorAxisLength','PixelIdxList');
-% Eliminate corals greater than a certain size
-stats = filterCoralsbySize(stats,cmThreshold,pixToCM);
-coralImage = logical(makeMaskFromStats(stats,size(Ibw)));
