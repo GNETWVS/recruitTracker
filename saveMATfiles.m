c6623643bc50666995a9d1dfe13ce3e231060616
dakkaynak <derya.akkaynak@gmail.com>
2017-05-31 16:52:21 +0300

    Delete saveMATfiles.m

--- saveMATfiles.m
+++
@@ -1,4 +0,0 @@
-function saveMATfiles(folderName,fileName, keepMask,rejectMask,addedMask,stats,resizeFactor,pix2CM)
-
-% save the masks and stats
-save([folderName,'/',fileName,'_masks.mat'],'keepMask','rejectMask','addedMask','stats','resizeFactor','pix2CM')
